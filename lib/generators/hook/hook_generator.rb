069db8327b8a364be1533f0a0ac8a1c5729aee39
Chuck Callebs <chuck@callebs.io>
2015-08-30 18:32:01 -0400

    Add hook generator scaffold.

---
+++ lib/generators/hook/hook_generator.rb
@@ -0,0 +1,10 @@
+module PokeyRunner
+  module Generators
+    class HookGenerator < Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      def add_hook
+      end
+    end
+  end
+end
