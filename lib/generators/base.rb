bb4d27570cd91dcb61c5c85af3e851fbdbfb65ed
Chuck Callebs <chuck@callebs.io>
2015-08-31 20:28:25 -0400

    Flesh out install generator.

--- lib/generators/base.rb
+++ lib/generators/base.rb
@@ -1,8 +1,23 @@
+require 'fileutils'
+
 module PokeyRunner
   module Generators
     class Base
       def self.source_root
         File.dirname(__FILE__)
+      end
+
+      # File railties/lib/rails_generator/commands.rb, line 298
+      def template(relative_source, relative_destination, template_options = {})
+        file(relative_source, relative_destination, template_options) do |file|
+          # Evaluate any assignments in a temporary, throwaway binding.
+          vars = template_options[:assigns] || {}
+          b = template_options[:binding] || binding
+          vars.each { |k,v| eval "#{k} = vars[:#{k}] || vars['#{k}']", b }
+
+          # Render the source file with the temporary binding.
+          ERB.new(file.read, nil, '-').result(b)
+        end
       end
     end
   end
--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -4,12 +4,15 @@ module PokeyRunner
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_config_file
+        template 'config.rb', 'config.rb'
       end
 
       def add_hook_dir
+        FileUtils::mkdir_p 'hooks'
       end
 
       def add_default_hook
+        template 'custom_hook.rb', 'hooks/custom_hook.rb'
       end
     end
   end
